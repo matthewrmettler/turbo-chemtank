319caba79695d488594f4c7b4dfa5084164a7047
Matthew Mettler <matthewrmettler@gmail.com>
2020-12-12 21:14:51 -0500

    Fix imports

--- summoner.py
+++ summoner.py
@@ -1,10 +1,9 @@
 import json
 
-import cassiopeia as cass
 from cassiopeia import Summoner
 
-from helper/matches import match_summary
-from helper/champs import mastery_summary
+from helper.matches import match_summary
+from helper.champs import mastery_summary
 
 
 def print_summoner(name: str, region: str):
@@ -14,8 +13,7 @@ def print_summoner(name: str, region: str):
     print("Account ID:", summoner.account_id)
     print("Level:", summoner.level)
     print("Revision date:", summoner.revision_date)
-    #print("Profile icon ID:", summoner.profile_icon.id)
-    print("Profile icon name:", summoner.profile_icon.name)
+
 
 
 def test(event, context):
@@ -23,25 +21,33 @@ def test(event, context):
 
 
 def get_summary(event, context):
-    summoner_name = event.get('pathParameters').get('summoner_name')
-    summoner_region = event.get('pathParameters').get('summoner_region') or 'NA'
+    try:
+        summoner_name = event.get('pathParameters').get('summoner_name')
+        summoner_region = event.get('pathParameters').get('summoner_region') or 'NA'
 
-    summoner = Summoner(name=summoner_name, region=summoner_region)
+        summoner = Summoner(name=summoner_name, region=summoner_region)
 
-    body = {
-        "account_id": summoner.account_id,
-        "level": summoner.level,
-        "matches": match_summary(summoner),
-        "mastery": mastery_summary(summoner)
-    }
+        body = {
+            "account_id": summoner.account_id,
+            "level": summoner.level,
+            "matches": match_summary(summoner),
+            "mastery": mastery_summary(summoner)
+        }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+
+
+    except Exception as e:
+        error_msg = "Unexpected error: %s" % str(e)
+        response = {
+            "statusCode": 500,
+            "body": json.dumps(error_msg)
+        }
 
     return response
-
 
 if __name__ == "__main__":
     print_summoner("Matty The Sloth", "NA")
